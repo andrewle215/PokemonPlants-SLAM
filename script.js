2b1af167c0b14c5fabbc3920001c87579303b070
nhat82 <doristrungvuong@gmail.com>
2025-03-11 20:22:10 -0400

    nvf

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.onload = () => {
                         const placeText = document.createElement('a-link');
                         placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
                         placeText.setAttribute('title', place.name);
-                        placeText.setAttribute('scale', '0.5 0.5 0.5');
+                        placeText.setAttribute('scale', '15 15 15');
 
                         placeText.addEventListener('loaded', () => {
                             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'));

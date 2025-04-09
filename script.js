91dd613f6f1137651e4274b734756b0e6c2b68d4
Andrew <andrewle2003@gmail.com>
2025-04-09 18:51:02 -0400

    offset test 90

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ window.onload = () => {
   const debugInfo = document.getElementById('debug-info');
 
   const isIOS = /iPhone|iPad|iPod/i.test(navigator.userAgent);
-  const offset = isIOS ? 0 : 0;
+  const offset = isIOS ? 90 : 0;
   debugInfo.textContent = `Offset applied: ${offset}° (${isIOS ? 'iOS' : 'Default'})`;
 
   // 🎯 Heading tracker
